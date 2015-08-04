e2025832108a0ffef2a656547c0f201c3290e088
Tarik Kelestemur <tkelestemur@gmail.com>
2015-08-03 23:11:24 -0400

    first test for motor control

--- src/sim_start.cpp
+++ src/sim_start.cpp
@@ -1,33 +1,42 @@
-#include "ros/ros.h"
 
+// ros libs
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+// vrep libs
 #include "vrep_common/VrepInfo.h"
 #include "vrep_common/simRosStartSimulation.h"
 #include "vrep_common/simRosStopSimulation.h"
+#include "vrep_common/JointSetStateData.h"
+
+bool simulationRunning=true;
+float simulationTime=0.0f;
+
+void infoCallback(const vrep_common::VrepInfo::ConstPtr& info)
+{
+	simulationTime=info->simulationTime.data;
+	simulationRunning=(info->simulatorState.data&1)!=0;
+}
 
 void startStopSim(ros::NodeHandle n, int s){
 
-if (s == 0){
+  if (s == 0){
+    ros::ServiceClient client_simStart = n.serviceClient<vrep_common::simRosStartSimulation>("/vrep/simRosStartSimulation");
+    vrep_common::simRosStartSimulation srv_simStart;
+    if(client_simStart.call(srv_simStart)){
+      ROS_INFO("Started"); // TODO check if service call response is correct
+    }
 
-ros::ServiceClient client_simStart = n.serviceClient<vrep_common::simRosStartSimulation>("/vrep/simRosStartSimulation");
-vrep_common::simRosStartSimulation srv_simStart;
-if(client_simStart.call(srv_simStart)){
-ROS_INFO("Started"); // TODO check if service call response is correct
-}
+  }
 
-}
+  else if (s == 1){
+    ros::ServiceClient client_simStop = n.serviceClient<vrep_common::simRosStopSimulation>("/vrep/simRosStopSimulation");
+    vrep_common::simRosStopSimulation srv_simStop;
+    if(client_simStop.call(srv_simStop)){
+      ROS_INFO("Started"); // TODO check if service call response is correct
+    }
 
-else if (s == 1){
-
-ros::ServiceClient client_simStop = n.serviceClient<vrep_common::simRosStopSimulation>("/vrep/simRosStopSimulation");
-vrep_common::simRosStopSimulation srv_simStop;
-if(client_simStop.call(srv_simStop)){
-ROS_INFO("Started"); // TODO check if service call response is correct
-
-}
-
-}
-
-
+  }
 
 }
 
@@ -37,17 +46,46 @@ int main(int argc, char **argv)
 
   ros::init(argc, argv, "sim_start");
   ros::NodeHandle n;
+  std_msgs::String topicName;
+  topicName.data = "omni";
+
+	ros::Subscriber subInfo=node.subscribe("/vrep/info",1,infoCallback);
+  startStopSim(n,0);
+
+  ros::ServiceClient client_enableSubscriber=node.serviceClient<vrep_common::simRosEnableSubscriber>("/vrep/simRosEnableSubscriber");
+  vrep_common::simRosEnableSubscriber srv_enableSubscriber;
+
+  srv_enableSubscriber.request.topicName="/"+topicName.data.c_str()+"/wheels"; // the topic name
+  srv_enableSubscriber.request.queueSize=1; // the subscriber queue size (on V-REP side)
+  srv_enableSubscriber.request.streamCmd=simros_strmcmd_set_joint_state; // the subscriber type
+  if (client_enableSubscriber.call(srv_enableSubscriber)&&(srv_enableSubscriber.response.subscriberID!=-1) {
 
 
-//  ros::Rate loop_rate(10);
+    ros::Publisher motorSpeedPub=node.advertise<vrep_common::JointSetStateData>("wheels",1);
+    while (ros::ok() && simulationRunning) {
+      vrep_common::JointSetStateData motorSpeeds;
 
-	startStopSim(n,0);
+
+      motorSpeeds.handles.data.push_back(26);
+      motorSpeeds.setModes.data.push_back(2); // 2 is the speed mode
+      motorSpeeds.values.data.push_back(1.0);
+      motorSpeedPub.publish(motorSpeeds);
+
+    }
+  }
 
 
-    ros::spinOnce();
 
-//    loop_rate.sleep();
-  
+
+
+
+
+  //  ros::Rate loop_rate(10);
+
+  ros::spinOnce();
+
+  //    loop_rate.sleep();
+
 
 
   return 0;
